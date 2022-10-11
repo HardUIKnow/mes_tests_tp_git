316781f4715de6c378ca5c961603c1dbc8346a3c
HardUIKnow <harduiknow@gmail.com>
2022-10-11 10:51:18 +0200

    modification du fichier

--- test1.c
+++ test1.c
@@ -5,5 +5,7 @@ int main(int argc, char const *argv[])
     printf("hello world \n");
 
     printf("ceci est un exemple de tp git \n" );
+    printf("on met a jour ce fichier sur git \n" );
+    printf("bienvenue sur cette version 1.0 \n" );
     return 0;
 }
